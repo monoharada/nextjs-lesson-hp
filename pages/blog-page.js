28613287688e341c06eaf26104d4f8dc49e2c2dd
monoharada <harada@monosus.co.jp>
2021-06-07 08:59:29 +0000

    l-9

--- pages/blog-page.js
+++ pages/blog-page.js
@@ -1,11 +1,22 @@
 import Layout from "../components/Layout"
+import Post from "../components/Post"
+import {getAllPostData} from "../lib/posts"
 
-const Blog = () => {
+const Blog = ({posts}) => {
   return (
     <Layout title="Blog">
-      Blog Page
+      <ul className="m-10">
+        {posts && posts.map(post =><Post key={post.id} post={post} />)}
+      </ul>
     </Layout>
   )
 }
 
 export default Blog
+
+export async function getStaticProps() {
+  const posts = await getAllPostData()
+  return {
+    props:{posts}
+  }
+}
