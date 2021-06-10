e2e8dc52c0272c45d0f72057591792ed552ac2c0
monoharada <harada@monosus.co.jp>
2021-06-10 04:17:16 +0000

    auth test 21

--- next.config.js
+++ next.config.js
@@ -2,11 +2,7 @@
 
 module.exports = {
   trailingSlash: true,
-  distDir: 'build',
-  generateBuildId: async () => {
-    // You can, for example, get the latest git commit hash here
-    return 'my-build-id'
-  },
+
 };
 
 // module.exports = withOptimizedImages({
