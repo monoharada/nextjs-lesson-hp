6ae83d8e330e09c22f9dc37050e27287255376be
monoharada <harada@monosus.co.jp>
2021-06-10 01:21:19 +0000

    auth test 4

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,8 @@
 import Head from 'next/head'
 import Link from 'next/link'
 
-import httpAuthCheck from '../util/httpAuthCheck'
 
-export async function getServerSideProps(ctx) {
-  const {req, res} = ctx
-  await httpAuthCheck(req, res)
 
-  if (!req.headers.authorization) {
-    res.end('<html>Unauthorized</html>')
-  }
-  return {
-    props: {}
-  }
-}
 
 export default function Layout({children,title="HP by next.js"}) {
   return (
