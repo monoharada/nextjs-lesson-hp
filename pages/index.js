05323a4f34933b34c1ca237d37e3b1b95fe509ae
monoharada <harada@monosus.co.jp>
2021-06-10 09:33:40 +0000

    ref:index.js

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,10 @@
 import Layout from "../components/Layout";
 
-// import httpAuthCheck from '../util/httpAuthCheck'
-
-// export async function getServerSideProps(ctx) {
-//   const {req, res} = ctx
-//   await httpAuthCheck(req, res)
-
-//   if (!req.headers.authorization) {
-//     res.end('<html>Unauthorized</html>')
-//   }
-//   return {
-//     props: {}
-//   }
-// }
 
 export default function Home() {
   return (
    <Layout title="Home">
-    <p className="text-4xl">Hello Nexxxxxxt.js</p>
+    <p className="text-4xl">Hello Next.js</p>
    </Layout>
 
   )
