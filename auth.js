38221c5bac7ae0e153f04a29cac4d614ef553a45
monoharada <harada@monosus.co.jp>
2021-06-10 02:35:24 +0000

    auth test 12

--- auth.js
+++ auth.js
@@ -7,7 +7,7 @@ const app = protect(
     safeCompare(username, process.env.USERNAME || 'admin') &&
     safeCompare(password, process.env.PASSWORD || 'admin'),
   {
-    directory: `/`,
+    // directory: `/`,
     onAuthFailed: (res) => {
       res.end('Authentication failed');
     },
