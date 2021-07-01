98c5000133bf4588f8f1756d0161a8a03e147e13
monoharada <harada@monosus.co.jp>
2021-07-01 01:57:21 +0000

    test up for auth

--- pages/index.js
+++ pages/index.js
@@ -3,9 +3,10 @@ import Layout from "../components/Layout";
 
 export default function Home() {
   return (
-   <Layout title="Home">
-    <p className="text-4xl">Hello Next.js</p>
-   </Layout>
+    <Layout title="Home">
+      <p className="text-4xl">Hello Next.js</p>
+      <p>TEST UP</p>
+    </Layout>
 
   )
 }
